c3406cb56604fe4500ba77f12edc0f43bb3cfeae
Andrew Devries <andrew@andrewdevries.net>
2025-11-18 12:55:36 -0600

    Merge in FindItem, Serialize, TimeZoneHeader

---
+++ ews/src/types/get_server_timezones.rs
@@ -0,0 +1,94 @@
+use ews_proc_macros::operation_response;
+use serde::Deserialize;
+use xml_struct::XmlSerialize;
+
+use crate::{
+    timezone::{TimeZoneDefinitions, TimeZoneId},
+    MESSAGES_NS_URI,
+};
+
+#[derive(Clone, Debug, Deserialize, XmlSerialize, PartialEq, Eq)]
+#[xml_struct(default_ns = MESSAGES_NS_URI)]
+#[serde(rename_all = "PascalCase")]
+#[operation_response(GetServerTimeZonesResponseMessage)]
+pub struct GetServerTimeZones {
+    #[xml_struct(attribute)]
+    #[serde(rename = "@ReturnFullTimeZoneData")]
+    pub return_full_time_zone_data: Option<bool>,
+
+    pub ids: Option<Vec<TimeZoneId>>,
+}
+
+#[derive(Clone, Debug, Deserialize, XmlSerialize, PartialEq, Eq)]
+#[serde(rename_all = "PascalCase")]
+pub struct GetServerTimeZonesResponseMessage {
+    pub time_zone_definitions: TimeZoneDefinitions,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        test_utils::{assert_deserialized_content, assert_serialized_content},
+        timezone::{Periods, TimeZoneDefinition, TransitionsGroups},
+        ResponseClass, ResponseMessages,
+    };
+
+    use super::*;
+
+    #[test]
+    fn serialize_get_server_time_zones_request() {
+        let get_server_timezones = GetServerTimeZones {
+            return_full_time_zone_data: Some(true),
+            ids: Some(vec![TimeZoneId {
+                id: "Eastern Standard Time".to_string(),
+            }]),
+        };
+
+        let expected = r#"<GetServerTimeZones xmlns="http://schemas.microsoft.com/exchange/services/2006/messages" ReturnFullTimeZoneData="true"><Ids><t:Id>Eastern Standard Time</t:Id></Ids></GetServerTimeZones>"#;
+
+        assert_serialized_content(&get_server_timezones, "GetServerTimeZones", expected);
+    }
+
+    #[test]
+    fn deserialize_get_server_time_zones_response() {
+        let content = r#"
+            <GetServerTimeZonesResponse
+                xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types"
+                xmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages">
+                <m:ResponseMessages>
+                <m:GetServerTimeZonesResponseMessage ResponseClass="Success">
+                    <m:ResponseCode>NoError</m:ResponseCode>
+                    <m:TimeZoneDefinitions>
+                    <t:TimeZoneDefinition Name="(UTC) Coordinated Universal Time" Id="UTC">
+                        <t:Periods />
+                        <t:TransitionsGroups />
+                    </t:TimeZoneDefinition>
+                    </m:TimeZoneDefinitions>
+                </m:GetServerTimeZonesResponseMessage>
+                </m:ResponseMessages>
+            </GetServerTimeZonesResponse>"#;
+
+        let expected = GetServerTimeZonesResponse {
+            response_messages: ResponseMessages {
+                response_messages: vec![ResponseClass::Success(
+                    GetServerTimeZonesResponseMessage {
+                        time_zone_definitions: TimeZoneDefinitions {
+                            inner: vec![TimeZoneDefinition {
+                                id: "UTC".to_string(),
+                                name: Some(r#"(UTC) Coordinated Universal Time"#.to_string()),
+                                periods: Periods { period: vec![] },
+                                transitions_groups: TransitionsGroups {
+                                    id: None,
+                                    transitions: vec![],
+                                },
+                                transitions: None,
+                            }],
+                        },
+                    },
+                )],
+            },
+        };
+
+        assert_deserialized_content(content, expected);
+    }
+}
---
+++ ews/src/types/timezone.rs
@@ -0,0 +1,135 @@
+use serde::Deserialize;
+use xml_struct::XmlSerialize;
+
+#[derive(Clone, Debug, Deserialize, XmlSerialize, PartialEq, Eq)]
+#[serde(rename_all = "PascalCase")]
+pub struct TimeZoneDefinitions {
+    #[serde(rename = "$value", default)]
+    pub inner: Vec<TimeZoneDefinition>,
+}
+
+#[derive(Clone, Debug, Deserialize, XmlSerialize, PartialEq, Eq)]
+#[serde(rename_all = "PascalCase")]
+pub struct TimeZoneDefinition {
+    /// Represents the unique identifier of the time zone.
+    #[xml_struct(attribute)]
+    #[serde(rename = "@Id")]
+    pub id: String,
+
+    /// Represents the descriptive name of the time zone.
+    #[xml_struct(attribute)]
+    #[serde(rename = "@Name", skip_serializing_if = "Option::is_none")]
+    pub name: Option<String>,
+
+    pub periods: Periods,
+
+    pub transitions_groups: TransitionsGroups,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub transitions: Option<Transitions>,
+}
+
+impl Default for TimeZoneDefinition {
+    fn default() -> Self {
+        Self {
+            id: "UTC".to_string(),
+            name: Default::default(),
+            periods: Periods {
+                period: Default::default(),
+            },
+            transitions_groups: TransitionsGroups {
+                id: None,
+                transitions: Default::default(),
+            },
+            transitions: Default::default(),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, XmlSerialize, PartialEq, Eq)]
+pub struct Periods {
+    #[serde(rename = "$value", default)]
+    pub period: Vec<Period>,
+}
+
+#[derive(Clone, Debug, Deserialize, XmlSerialize, PartialEq, Eq)]
+pub struct Period {
+    #[xml_struct(attribute)]
+    #[serde(rename = "@Id")]
+    pub id: String,
+
+    #[xml_struct(attribute)]
+    #[serde(rename = "@Bias")]
+    pub bias: String,
+
+    #[xml_struct(attribute)]
+    #[serde(rename = "@Name")]
+    pub name: String,
+}
+
+#[derive(Clone, Debug, Deserialize, XmlSerialize, PartialEq, Eq)]
+#[serde(rename_all = "PascalCase")]
+pub struct TransitionsGroups {
+    #[xml_struct(attribute)]
+    #[serde(rename = "@Id")]
+    pub id: Option<String>,
+
+    #[serde(rename = "$value", default)]
+    pub transitions: Vec<Transitions>,
+}
+
+#[derive(Clone, Debug, Deserialize, XmlSerialize, PartialEq, Eq)]
+#[serde(rename_all = "PascalCase")]
+pub struct Transitions {
+    #[serde(rename = "$value", default)]
+    pub transitions: Vec<Transition>,
+}
+
+#[derive(Clone, Debug, Deserialize, XmlSerialize, PartialEq, Eq)]
+#[serde(rename_all = "PascalCase")]
+pub enum Transition {
+    AbsoluteDateTransition(AbsoluteDateTransition),
+    RecurringDayTransition(RecurringDayTransition),
+    RecurringDateTransition(RecurringDateTransition),
+}
+
+#[derive(Clone, Debug, Deserialize, XmlSerialize, PartialEq, Eq)]
+#[serde(rename_all = "PascalCase")]
+pub struct AbsoluteDateTransition {
+    pub to: String,
+
+    pub date_time: String,
+}
+
+#[derive(Clone, Debug, Deserialize, XmlSerialize, PartialEq, Eq)]
+#[serde(rename_all = "PascalCase")]
+pub struct RecurringDayTransition {
+    pub to: String,
+
+    pub time_offset: String,
+
+    pub month: usize,
+
+    pub day_of_week: String,
+
+    pub occurrence: usize,
+}
+
+#[derive(Clone, Debug, Deserialize, XmlSerialize, PartialEq, Eq)]
+#[serde(rename_all = "PascalCase")]
+pub struct RecurringDateTransition {
+    pub to: String,
+
+    pub time_offset: String,
+
+    pub month: usize,
+
+    pub day: usize,
+}
+
+/// Identifies a single time zone definition.
+#[derive(Clone, Debug, Deserialize, XmlSerialize, PartialEq, Eq)]
+pub struct TimeZoneId {
+    #[xml_struct(ns_prefix = "t")]
+    pub id: String,
+}
